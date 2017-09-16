384d277c55fb9244261590cf273ffaff66157232
ndevagiri6 <ndevagiri6@gmail.com>
2017-09-16 08:37:57 +0000

    Hello

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -7,4 +7,33 @@ app.config(function($mdThemingProvider) {
 
 app.controller('chatController', function ($scope){
 
+	$scope.messages = [
+	{
+		sender: "BOT",
+		text:"Hello There,What do you want to know?",
+		time:"13:12"
+	},
+	{
+		sender: "User",
+		text:"Hi, I wanted to check on weather.",
+		time:"13:12"
+	},
+	{
+		sender: "BOT",
+		text:"Today the weather in Bengaluru is 36 C.",
+		time:"13:13"
+	},
+	
+	{
+		sender: "User",
+		text:"Thanks.",
+		time:"13:14"
+	}];
+
+	var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+	exampleSocket.onmessage  =   function  (event) {
+       var jsonData = JSON.parse(event.data);
+       console.log(jsonData);    
+   };
+
 });
